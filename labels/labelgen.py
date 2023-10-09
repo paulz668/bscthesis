8425d4c73272f9379e3300bd24baa06a4b18efe1
paulz668 <paul.znidaric@outlook.com>
2023-10-09 18:52:16 +0200

    Script for Label generation

--- labels/labelgen.py
+++ labels/labelgen.py
@@ -1,4 +1,45 @@
 import numpy as np
 import pandas as pd
 
-data_wA = pd.read_csv('C:/Users/paulz/Documents/Financial_Literacy/Investing/Python trading bot/LOB_data/LOB_Data/Auction/1.Auction_Zscore/Auction_Zscore_Testing/Test_Dst_Auction_ZScore_CF_1')
+def EWMSD(df, n, span0 = 100):
+    """
+    calculate exponentially weighted rolling standard deviation with lookback period span0
+    and sampling frequency n 
+    """
+    return None
+
+def STBL(mp, ewmsd, n):
+    """
+    label observation at time t based on the static triple barrier method
+    mp is an array like container of midprices from time t to t+n
+    ewmsd is the exponentially weighted moving standard deviation at time t for sampling frequency n
+    n is the number of periods the forecast looks into the future 
+    1 signifies an up move, 2 a stationary move and 3 a down move
+    """
+    for i in range(len(mp)):
+        if i == 0:
+            continue
+        if mp[i] > (mp[0] + ewmsd):
+            return 1
+        elif mp[i] < (mp[0] + ewmsd):
+            return 3
+    return 2
+
+def DTBL(mp, ewmsd, n):
+    """
+    label observation at time t based on the dynamic triple barrier method
+    mp is an array like container of midprices from time t to t+n
+    ewmsd is an array like container of exponentially weighted moving standard deviation at time t
+    for the sampling frequencies from 1 to n
+    n is the number periods the forecast looks into the future
+    1 signifies an up move, 2 a stationary move and 3 a down move
+    """
+    for i in range(len(mp)):
+        if i == 0:
+            continue
+        if mp[i] > (mp[0] + ewmsd[i-1]):
+            return 1
+        elif mp[i] < (mp[0] +ewmsd[i-1]):
+            return 3
+    return 2
+    
