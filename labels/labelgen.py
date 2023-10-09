872943c54f421f116039cf9bcfab5637e23def9d
paulz668 <paul.znidaric@outlook.com>
2023-10-09 18:21:22 +0200

    Create labelgen.py

---
+++ labels/labelgen.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+
+data_wA = pd.read_csv('C:/Users/paulz/Documents/Financial_Literacy/Investing/Python trading bot/LOB_data/LOB_Data/Auction/1.Auction_Zscore/Auction_Zscore_Testing/Test_Dst_Auction_ZScore_CF_1')
