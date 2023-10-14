e13bd766c3eaf19e8e6a5c75bf9bda62cded3663
paulz668 <paul.znidaric@outlook.com>
2023-10-14 22:56:40 +0200

    label generation for data

--- labels/labelgen.py
+++ labels/labelgen.py
@@ -1,63 +1,63 @@
 import numpy as np
 import math
 
-def ew(mp, a = 34.65620375):
+def ew(mpr, a = 34.65620375):
     """
-    calculate exponentially decaying weights based on array size of midprices
-    de Prado (2017) suggests defining decay using a span of 100 which is equivalent to 34.65620375 when 
-    defining decay in terms of halflife
+    calculate exponentially decaying weights based on array size of midprice returns
+    de Prado (2018) suggests defining decay using a span of 100 which is equivalent to a halflife of 34.65620375 
+    when defining decay in terms of halflife
     """
-    return 1 + np.exp((-np.log(2) / a) * np.linspace(1, mp.size, mp.size, endpoint = True))
+    return 1 + np.exp((-np.log(2) / a) * np.linspace(1, mpr.size, mpr.size, endpoint = True))
 
-def wsd(mp, w):
+def wsd(mpr, w):
     """
-    calculate the weighted standard deviation based on an arrray of mid prices mp and an array
+    calculate the weighted standard deviation based on an arrray of midprice returns mp and an array
     of weights w
     """
-    avg = np.average(mp, weights=w)
-    var = np.average((mp-avg)**2, weights=w)
+    avg = np.average(mpr, weights=w)
+    var = np.average((mpr-avg)**2, weights=w)
     return math.sqrt(var)
 
-def ewmsd(mp, n, a = 34.65620375):
+def ewmsd(mpr, n, a = 34.65620375):
     """
-    calculate exponentially weighted rolling standard deviation with sampling frequency n 
+    calculate exponentially weighted rolling standard deviation of mpr with sampling frequency n 
     """
-    w = ew(mp, a)
-    ewmsd = np.empty(mp.size)
-    for i in range(mp.size):
-        ewmsd[i] = wsd(mp[:i+1][::-n], w[:int(np.ceil((i+1)/n))][::-1])
+    w = ew(mpr, a)
+    ewmsd = np.empty(mpr.size)
+    for i in range(mpr.size):
+        ewmsd[i] = wsd(mpr[:i+1][::-n], w[:int(np.ceil((i+1)/n))][::-1])
     return ewmsd
 
-def stbl(mp, ewmsd):
+def stbl(mpr, ewmsd):
     """
     label observation at time t based on the static triple barrier method
-    mp is an array like container of midprices from time t to t+n
+    mpr is an array like container of midprice returns from time t to t+n
     ewmsd is the exponentially weighted moving standard deviation at time t for sampling frequency n 
     1 signifies an up move, 2 a stationary move and 3 a down move
     """
-    for i in range(mp.size):
+    for i in range(mpr.size):
         if i == 0:
             continue
-        if mp[i] > (mp[0] + ewmsd):
+        if mpr[i] > (mpr[0] + ewmsd):
             return 1
-        elif mp[i] < (mp[0] - ewmsd):
+        elif mpr[i] < (mpr[0] - ewmsd):
             return 3
     return 2
 
-def dtbl(mp, ewmsd):
+def dtbl(mpr, ewmsd):
     """
     label observation at time t based on the dynamic triple barrier method
-    mp is an array like container of midprices from time t to t+n (length n+1)
+    mpr is an array like container of midprice returns from time t to t+n (length n+1)
     ewmsd is an array like container of exponentially weighted moving standard deviation at time t
     for the sampling frequencies from 1 to n (length n)
     n is the number periods the forecast looks into the future
     1 signifies an up move, 2 a stationary move and 3 a down move
     """
-    for i in range(len(mp)):
+    for i in range(mpr.size):
         if i == 0:
             continue
-        if mp[i] > (mp[0] + ewmsd[i-1]):
+        if mpr[i] > (mpr[0] + ewmsd[i-1]):
             return 1
-        elif mp[i] < (mp[0] - ewmsd[i-1]):
+        elif mpr[i] < (mpr[0] - ewmsd[i-1]):
             return 3
     return 2
--- labels/labels.py
+++ labels/labels.py
@@ -2,8 +2,29 @@ import numpy as np
 import os
 from labelgen import *
 
-cwd = os.getcwd()
+cwd = "C:/Users/paulz/Documents/UNI/BBE/7. Semester/Bachelorarbeit/LOB_DATA" # current work directory
+n = [1,2,3,4,5,6,7,8,9,10] # sampling frequencies
 
 for root, dirs, files in os.walk(cwd):
     for name in files:
-        print(os.path.join(root, name))
+        data = np.loadtxt(os.path.join(root, name).replace("\\", "/")) # load data
+        mpr = np.diff(data[50, :], prepend=data[50, 0]) / data[50, :] # calculate simple returns for midprices
+        mprsd = np.empty((10, mpr.size))
+        for i in range(len(n)):
+            mprsd[i,:] = ewmsd(mpr, n[i])
+        labels = np.empty((10, mpr.size))
+        for i in range(mpr.size):
+            # static tripple barrier labels
+            labels[0, i] = stbl(mpr[i:i+2], mprsd[0, i])
+            labels[1, i] = stbl(mpr[i:i+3], mprsd[1, i])
+            labels[2, i] = stbl(mpr[i:i+4], mprsd[2, i])
+            labels[3, i] = stbl(mpr[i:i+6], mprsd[4, i])
+            labels[4, i] = stbl(mpr[i:i+11], mprsd[9, i])
+
+            # dynamic triple barrier labels
+            labels[5, i] = dtbl(mpr[i:i+2], mprsd[0, i])
+            labels[6, i] = dtbl(mpr[i:i+3], mprsd[:2, i])
+            labels[7, i] = dtbl(mpr[i:i+4], mprsd[:3, i])
+            labels[8, i] = dtbl(mpr[i:i+6], mprsd[:5, i])
+            labels[5, i] = dtbl(mpr[i:i+11], mprsd[:, i])
+        np.savetxt(name + '_NL', np.vstack((data, labels)))
